049d419d732326bdd32b93de7260cd5430f41937
Vadditandra Dileep Kumar <dileepkumarvadditandra@gmail.com>
2025-07-15 18:23:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Enter a number:");
+            a=int.Parse(Console.ReadLine());
+            Console.Write("Enter b number:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("number before swapping:" + a +","+ b);
+
+            c = a;
+            a=b;
+            b=c;
+            Console.WriteLine("swapped numbers:");
+            Console.WriteLine("Value of a:" + a);
+            Console.WriteLine("Value of b:" + b); 
+            Console.Read();
+
+
+
+        }
+    }
+}
